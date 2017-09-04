2175a56c1d6fd7df5bca600cfc7e1ecd7707c73a
Cal <callen-johnson@utulsa.edu>
2017-09-04 16:19:16 -0500

    Add some code to read in two more files

--- csv_reader.py
+++ csv_reader.py
@@ -6,8 +6,25 @@ Udacity data analysis course instructions.
 
 import unicodecsv
 
+# open the enrollments data file as a dictionary
 with open('enrollments.csv', "rb") as f:
     reader = unicodecsv.DictReader(f)
     enrollments = list(reader)
+
+# open the daily engagements data file as a dictionary
+with open('daily_engagement.csv', "rb") as f:
+    reader = unicodecsv.DictReader(f)
+    daily_engagement = list(reader)
+
+# open the project submission data file as a dictionary
+with open('project_submissions.csv', "rb") as f:
+    reader = unicodecsv.DictReader(f)
+    project_submissions = list(reader)
     
-enrollments[0]
+# view first row of each table
+print('\n', enrollments[0])
+print('\n', daily_engagement[0])
+print('\n', project_submissions[0])
+
+
+
